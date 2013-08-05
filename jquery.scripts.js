2e16c683bf33d8cfb1ba49f0f0137ee5ae6d9554
petertait <contact@petertait.com>
2013-08-05 18:28:24 +0100

    Colour
    First push of the Colour hack day microsite.

---
+++ jquery.scripts.js
@@ -0,0 +1,49 @@
+$(window).load(function () {
+	function findPos(obj) {
+	    var curleft = 0, curtop = 0;
+	    if (obj.offsetParent) {
+	        do {
+	            curleft += obj.offsetLeft;
+	            curtop += obj.offsetTop;
+	        } while (obj = obj.offsetParent);
+	        return { x: curleft, y: curtop };
+	    }
+	    return undefined;
+	}
+	
+	function rgbToHex(r, g, b) {
+	    if (r > 255 || g > 255 || b > 255)
+	        throw "Invalid color component";
+	    return ((r << 16) | (g << 8) | b).toString(16);
+	}
+	//mousemove
+	$('#canvas').click(function(e) {
+	    var pos = findPos(this);
+	    var x = e.pageX - pos.x;
+	    var y = e.pageY - pos.y;
+	    var coord = "x=" + x + ", y=" + y;
+	    var c = this.getContext('2d');
+	    var p = c.getImageData(x, y, 1, 1).data; 
+	    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+	    $('#hex').html(hex);
+	    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+	    $('.hex-code-colour').css('background-color', hex);
+	});
+	
+	$('#new-canvas').click(function(){location.reload();});
+	$('#hex-eyedrop').click(function() {
+		$('body').toggleClass('eyedrop-active');
+		$('#canvas').toggleClass('eyedrop-active');
+	});
+	function selectText(containerid) {
+        if (document.selection) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(document.getElementById(containerid));
+            range.select();
+        } else if (window.getSelection) {
+            var range = document.createRange();
+            range.selectNode(document.getElementById(containerid));
+            window.getSelection().addRange(range);
+        }
+    }
+});
